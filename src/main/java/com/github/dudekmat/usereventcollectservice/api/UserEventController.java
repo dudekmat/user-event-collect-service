8e49374659ed39f4bdb0a40b134513a327d04123
dudekmat <matdudek2706@gmail.com>
2023-03-25 01:26:03 +0100

    Changed request body to list

--- src/main/java/com/github/dudekmat/usereventcollectservice/api/UserEventController.java
+++ src/main/java/com/github/dudekmat/usereventcollectservice/api/UserEventController.java
@@ -5,6 +5,7 @@ import com.github.dudekmat.usereventcollectservice.event.UserEventProducer;
 import com.github.dudekmat.usereventcollectservice.event.UserProductEvent;
 import com.github.dudekmat.usereventcollectservice.event.UserSearchEvent;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,32 +22,32 @@ class UserEventController {
   private final UserEventProducer userEventProducer;
 
   @PostMapping("/product-events")
-  void collectProductEvents(@RequestBody @Valid UserProductEventPayload userProductEventPayload) {
-    publishUserEvent(UserProductEvent.builder()
-        .eventType(userProductEventPayload.eventType())
-        .userId(userProductEventPayload.userId())
-        .sessionId(userProductEventPayload.sessionId())
-        .eventTime(userProductEventPayload.eventTime())
-        .productId(userProductEventPayload.productId())
-        .pageName(userProductEventPayload.pageName())
-        .boxName(userProductEventPayload.boxName())
-        .boxPosition(userProductEventPayload.boxPosition())
-        .platform(userProductEventPayload.platform())
-        .build());
+  void collectProductEvents(@RequestBody @Valid List<UserProductEventPayload> userProductEvents) {
+    userProductEvents.forEach(event -> publishUserEvent(UserProductEvent.builder()
+        .eventType(event.eventType())
+        .userId(event.userId())
+        .sessionId(event.sessionId())
+        .eventTime(event.eventTime())
+        .productId(event.productId())
+        .pageName(event.pageName())
+        .boxName(event.boxName())
+        .boxPosition(event.boxPosition())
+        .platform(event.platform())
+        .build()));
   }
 
   @PostMapping("/search-events")
-  void collectSearchEvents(@RequestBody @Valid UserSearchEventPayload userSearchEventPayload) {
-    publishUserEvent(UserSearchEvent.builder()
-        .eventType(userSearchEventPayload.eventType())
-        .userId(userSearchEventPayload.userId())
-        .sessionId(userSearchEventPayload.sessionId())
-        .eventTime(userSearchEventPayload.eventTime())
-        .productId(userSearchEventPayload.productId())
-        .query(userSearchEventPayload.query())
-        .searchParams(userSearchEventPayload.searchParams())
-        .platform(userSearchEventPayload.platform())
-        .build());
+  void collectSearchEvents(@RequestBody @Valid List<UserSearchEventPayload> userSearchEvents) {
+    userSearchEvents.forEach(event -> publishUserEvent(UserSearchEvent.builder()
+        .eventType(event.eventType())
+        .userId(event.userId())
+        .sessionId(event.sessionId())
+        .eventTime(event.eventTime())
+        .productId(event.productId())
+        .query(event.query())
+        .searchParams(event.searchParams())
+        .platform(event.platform())
+        .build()));
   }
 
   private void publishUserEvent(UserEvent userEvent) {
